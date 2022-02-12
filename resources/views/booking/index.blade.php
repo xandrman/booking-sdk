c576cd44a6cc7fb9e06796b17bd4412349dc515a
xandrman <xandrman@yandex.ru>
2022-02-13 03:12:14 +0600

    Таблицы table-responsive

--- resources/views/booking/index.blade.php
+++ resources/views/booking/index.blade.php
@@ -17,28 +17,28 @@
 @endsection
 
 @section('content')
-    <table class="table table-bordered table-hover">
-        <thead class="thead-light">
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">Имя клиента</th>
-            <th scope="col">E-mail клиента</th>
-            <th scope="col">Начало</th>
-            <th scope="col">Конец</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($bookings as $booking)
+    <div class="table-responsive">
+        <table class="table table-bordered table-hover">
+            <thead class="thead-light">
             <tr>
-                <th scope="row">{{ $booking['id'] }}</th>
-                <td>{{ $booking['customer']['name'] }}</td>
-                <td>{{ $booking['customer']['email'] }}</td>
-                <td>{{ $booking['from'] }}</td>
-                <td>{{ $booking['to'] }}</td>
+                <th scope="col">id</th>
+                <th scope="col">Имя клиента</th>
+                <th scope="col">E-mail клиента</th>
+                <th scope="col">Начало</th>
+                <th scope="col">Конец</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
-
-
+            </thead>
+            <tbody>
+            @foreach($bookings as $booking)
+                <tr>
+                    <th scope="row">{{ $booking['id'] }}</th>
+                    <td>{{ $booking['customer']['name'] }}</td>
+                    <td>{{ $booking['customer']['email'] }}</td>
+                    <td>{{ $booking['from'] }}</td>
+                    <td>{{ $booking['to'] }}</td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
--- resources/views/customer/index.blade.php
+++ resources/views/customer/index.blade.php
@@ -19,27 +19,29 @@
 
 @section('content')
     <div class="mb-2">Выберите клиента и нажмите на кнопку Создать бронирование</div>
-    <table class="table table-bordered table-hover">
-        <thead class="thead-light">
-        <tr>
-            <th scope="col">id</th>
-            <th scope="col">Имя</th>
-            <th scope="col">E-mail</th>
-            <th scope="col">Действия</th>
-        </tr>
-        </thead>
-        <tbody>
-        @foreach($customers as $customer)
+    <div class="table-responsive">
+        <table class="table table-bordered table-hover">
+            <thead class="thead-light">
             <tr>
-                <th scope="row">{{ $customer['id'] }}</th>
-                <td>{{ $customer['name'] }}</td>
-                <td>{{ $customer['email'] }}</td>
-                <td class="text-right">
-                    <a href="{{ route('create', ['customer_id' => $customer['id']]) }}"
-                       class="btn btn-sm btn-success">Создать бронирование</a>
-                </td>
+                <th scope="col">id</th>
+                <th scope="col">Имя</th>
+                <th scope="col">E-mail</th>
+                <th scope="col">Действия</th>
             </tr>
-        @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+            @foreach($customers as $customer)
+                <tr>
+                    <th scope="row">{{ $customer['id'] }}</th>
+                    <td>{{ $customer['name'] }}</td>
+                    <td>{{ $customer['email'] }}</td>
+                    <td class="text-right">
+                        <a href="{{ route('create', ['customer_id' => $customer['id']]) }}"
+                           class="btn btn-sm btn-success">Создать бронирование</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
